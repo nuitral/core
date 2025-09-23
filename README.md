98e3c712d5d58f280d73c8a4148a1c7c1c14f272
nicolacentonze <nicola.centonze@icloud.com>
2025-09-23 20:33:07 +0200

    First commit @nuitral/core

---
+++ README.md
@@ -0,0 +1,1 @@
+## nuitral Core
---
+++ src/my-element.ts
@@ -0,0 +1,20 @@
+import {LitElement, html} from 'lit'
+import {customElement} from 'lit/decorators.js'
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+
+    render() {
+        return html`
+            <div>
+                nuitral Core
+            </div>
+        `
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'my-element': MyElement
+    }
+}
